5db27be0e9b00f32c209dc56118aa9b923ac1fa7
Neil Mayhew <neil.mayhew@iohk.io>
2025-09-02 18:30:55 -0600

    Change default compiler in flake to ghc967 and add a ci package

--- flake.nix
+++ flake.nix
@@ -9,9 +9,14 @@
         import nixpkgs { inherit system; };
       let
         packages = lib.attrsets.mapAttrs (_: compiler: compiler.callPackage ./. { }) haskell.packages;
+        supported = { inherit (packages) ghc8107 ghc928 ghc948 ghc967 ghc984 ghc9102 ghc9122; };
+        default = packages.ghc967;
       in
       {
-        packages = packages // { default = packages.ghc910; };
+        packages = packages // {
+          inherit default;
+          ci = linkFarm "ci" supported;
+        };
         devShells = lib.attrsets.mapAttrs (_: p: p.env) self.packages.${system};
       }
     );
