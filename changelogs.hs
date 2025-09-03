a1612b904cf3379aca5b74b19a3221e2effc40fd
Neil Mayhew <neil.mayhew@iohk.io>
2025-09-03 13:47:27 -0600

    Make --inplace and --output mutually exclusive

--- changelogs.hs
+++ changelogs.hs
@@ -4,7 +4,6 @@
 
 import Changelog
 
-import Control.Monad (guard)
 import Data.Foldable (for_)
 import Data.Text (Text, unpack)
 import Options.Applicative
@@ -15,11 +14,9 @@ import qualified System.Console.Terminal.Size as TS
 
 data Options = Options
   { optChangelogs :: [String]
-  , optInplace :: Bool
-  , optOutput :: Maybe FilePath
+  , optWriteFile :: FilePath -> Text -> IO ()
   , optBulletHierarchy :: Text
   }
-  deriving (Show)
 
 main :: IO ()
 main = do
@@ -30,17 +27,22 @@ main = do
       (prefs $ columns cols)
       ( info
           ( helper <*> do
-              optInplace <-
-                switch $
-                  help "Modify files in-place"
-                    <> short 'i'
-                    <> long "inplace"
-              optOutput <-
-                optional . strOption $
-                  help "Write output to FILE"
-                    <> short 'o'
-                    <> long "output"
-                    <> metavar "FILE"
+              let
+                inplaceParser =
+                  flag' T.writeFile $
+                    help "Modify files in-place"
+                      <> short 'i'
+                      <> long "inplace"
+                fileParser =
+                  fmap (const . T.writeFile) . strOption $
+                    help "Write output to FILE"
+                      <> short 'o'
+                      <> long "output"
+                      <> metavar "FILE"
+                stdoutParser =
+                  -- Write output to stdout
+                  pure $ const T.putStrLn
+              optWriteFile <- inplaceParser <|> fileParser <|> stdoutParser
               optBulletHierarchy <-
                 strOption $
                   help "Use CHARS for the levels of bullets"
@@ -60,10 +62,9 @@ main = do
 
   for_ optChangelogs $ \fp -> do
     let
-      pError e = hPutStrLn stderr $ fp <> ": " <> e
-      pPrint = maybe T.putStr T.writeFile output . renderChangelog optBulletHierarchy
-      output = optOutput <|> (guard optInplace >> pure fp)
-    either pError pPrint =<< parseChangelogFile fp
+      printError e = hPutStrLn stderr $ fp <> ": " <> e
+      writeLog = optWriteFile fp . renderChangelog optBulletHierarchy
+    either printError writeLog =<< parseChangelogFile fp
 
 parseChangelogFile :: FilePath -> IO (Either String Changelog)
 parseChangelogFile f = parseChangelog <$> T.readFile f
